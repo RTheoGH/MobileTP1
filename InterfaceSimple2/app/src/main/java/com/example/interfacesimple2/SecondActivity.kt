c3620a43e26cc6e461ceb7f5d05c89f9be6b01ae
RTheoGH <reynier.theo@gmail.com>
2025-02-05 20:19:43 +0100

    Fin exercice 7

--- InterfaceSimple2/app/src/main/java/com/example/interfacesimple2/SecondActivity.kt
+++ InterfaceSimple2/app/src/main/java/com/example/interfacesimple2/SecondActivity.kt
@@ -2,7 +2,6 @@ package com.example.interfacesimple2
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
--- InterfaceSimple2/app/src/main/java/com/example/interfacesimple2/ThirdActivity.kt
+++ InterfaceSimple2/app/src/main/java/com/example/interfacesimple2/ThirdActivity.kt
@@ -1,51 +1,114 @@
 package com.example.interfacesimple2
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.core.content.ContextCompat
 import com.example.interfacesimple2.ui.theme.InterfaceSimple2Theme
-import com.example.interfacesimple2.R
 
 class ThirdActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val phone = intent.getStringExtra("phone")
+
             InterfaceSimple2Theme {
-                CenteredImage()
+                CenteredImage(phone!!)
             }
         }
     }
 }
 
 @Composable
-fun CenteredImage() {
-    // Utilisez un Box pour centrer l'image
-    Box(
-        modifier = Modifier.fillMaxSize(), // Box occupe tout l'espace disponible
-        contentAlignment = Alignment.Center // Centre tout son contenu
+fun CenteredImage(p:String) {
+    val ctx = LocalContext.current
+    val perm = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { grant ->
+        if(grant){
+            appel(ctx,p)
+        }
+    }
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.john_pork), // Image provenant des ressources
-            contentDescription = "Image description", // Description pour l'accessibilité
-            modifier = Modifier
-                .fillMaxSize(1.0f) // Agrandir l'image à 50% de la taille du conteneur
+        Text(
+            fontSize = 8.em,
+            text = p,
+            modifier = Modifier.padding(bottom = 10.dp)
         )
+        Image(
+            painter = painterResource(id = R.drawable.telephone),
+            contentDescription = "Image de téléphone",
+            modifier = Modifier.padding(vertical = 10.dp).fillMaxWidth(.9f)
+        )
+        IconButton(
+            modifier = Modifier.size(100.dp),
+            colors = IconButtonColors(
+                containerColor = Color(23, 128, 61),
+                contentColor = Color.White,
+                disabledContainerColor = Color.Gray,
+                disabledContentColor = Color.Black
+            ),
+            onClick = {
+                if (ContextCompat.checkSelfPermission(ctx,android.Manifest.permission.CALL_PHONE)
+                    == PackageManager.PERMISSION_GRANTED
+                ) {
+                    appel(ctx,p)
+                }else{
+                    perm.launch(android.Manifest.permission.CALL_PHONE)
+                }
+
+            }) {
+            Icon(
+                Icons.Rounded.Call,
+                contentDescription = "Bouton call",
+                modifier = Modifier.size(75.dp)
+            )
+        }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    InterfaceSimple2Theme {
-        CenteredImage()
+fun appel(ctx:Context, p:String){
+    val intentCall = Intent(Intent.ACTION_CALL).apply {
+        data = Uri.parse("tel:$p")
+    }
+
+    try {
+        ctx.startActivity(intentCall)
+    } catch (e: Exception) {
+        e.printStackTrace()
     }
 }
