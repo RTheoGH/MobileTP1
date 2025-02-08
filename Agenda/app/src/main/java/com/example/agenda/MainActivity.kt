194f2e648ebea61958f6a85dcae19c1337f6f53c
TheoR <reynier.theo@gmail.com>
2025-02-09 00:58:50 +0100

    page event

--- Agenda/app/src/main/java/com/example/agenda/MainActivity.kt
+++ Agenda/app/src/main/java/com/example/agenda/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.agenda
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -16,7 +24,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.agenda.ui.theme.AgendaTheme
 
@@ -24,6 +32,9 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val date = intent?.getStringExtra("date")?:""
+        val nom = intent?.getStringExtra("nom")?:""
+        val desc = intent?.getStringExtra("desc")?:""
         enableEdgeToEdge()
         setContent {
             AgendaTheme {
@@ -49,6 +60,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Agenda(innerPadding:PaddingValues) {
-    Column(modifier = Modifier.padding(innerPadding).padding(10.dp)) {
+
+    val ctx = LocalContext.current
+
+    Column(modifier = Modifier.padding(innerPadding).padding(16.dp)){
+        IconButton(
+            onClick = {
+                val intent = Intent(ctx,SecondActivity::class.java)
+                ctx.startActivity(intent)
+            }
+        ) {
+            Icon(
+                Icons.Rounded.Add,
+                contentDescription = "Add Event"
+            )
+        }
+
+        LazyColumn() {
+
+        }
     }
 }
+
+
