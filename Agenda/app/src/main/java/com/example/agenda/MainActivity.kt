69cfc5a490697121e73b93fe9bf5f09bb99ef7dc
TheoR <reynier.theo@gmail.com>
2025-02-08 18:54:45 +0100

    test agenda

--- Agenda/app/src/main/java/com/example/agenda/MainActivity.kt
+++ Agenda/app/src/main/java/com/example/agenda/MainActivity.kt
@@ -8,8 +8,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,12 +21,25 @@ import androidx.compose.ui.unit.dp
 import com.example.agenda.ui.theme.AgendaTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AgendaTheme {
-                Scaffold{ innerPadding ->
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text("Agenda")
+                            }
+                        )
+                    }
+                ){ innerPadding ->
                     Agenda(innerPadding)
                 }
             }
@@ -32,5 +49,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Agenda(innerPadding:PaddingValues) {
-
+    Column(modifier = Modifier.padding(innerPadding).padding(10.dp)) {
+    }
 }
