75e455103a1196d2924a143991a00bdb2ed9bedb
RTheoGH <reynier.theo@gmail.com>
2025-02-10 15:23:24 +0100

    Amélioration code interface kotlin

--- InterfaceSimple2/app/src/main/java/com/example/interfacesimple2/SecondActivity.kt
+++ InterfaceSimple2/app/src/main/java/com/example/interfacesimple2/SecondActivity.kt
@@ -5,53 +5,96 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.interfacesimple2.ui.theme.InterfaceSimple2Theme
+
+data class Infos(
+    val last_name: String?,
+    val first_name: String?,
+    val age: String?,
+    val domain: String?,
+    val phone: String?
+)
 
 class SecondActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val last_name = intent.getStringExtra("lastname")
-            val first_name = intent.getStringExtra("firstname")
-            val age = intent.getStringExtra("age")
-            val domain = intent.getStringExtra("domain")
-            val phone = intent.getStringExtra("phone")
+            val infos = Infos(
+                last_name = intent.getStringExtra("lastname"),
+                first_name = intent.getStringExtra("firstname"),
+                age = intent.getStringExtra("age"),
+                domain = intent.getStringExtra("domain"),
+                phone = intent.getStringExtra("phone")
+            )
 
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(text = stringResource(R.string.last_name) + " " + last_name)
-                Text(text = stringResource(R.string.first_name)+ " " + first_name)
-                Text(text = stringResource(R.string.age)+ " " + age)
-                Text(text = stringResource(R.string.domain)+ " " + domain)
-                Text(text = stringResource(R.string.phone)+ " " + phone)
-
-                val intent1 = Intent(this@SecondActivity,MainActivity::class.java).apply{}
-                val intent2 = Intent(this@SecondActivity,ThirdActivity::class.java).apply{
-                    putExtra("lastname",last_name)
-                    putExtra("firstname",first_name)
-                    putExtra("age",age)
-                    putExtra("domain", domain)
-                    putExtra("phone",phone)
-                }
-
-                Button(onClick = {
-                    startActivity(intent2)
-                }) {
-                    Text(text = stringResource(R.string.ok_button))
-                }
-                Button(onClick = {
-                    startActivity(intent1)
-                }) {
-                    Text(text = stringResource(R.string.return_button))
+            InterfaceSimple2Theme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text(text = stringResource(R.string.second_title))
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    Information(innerPadding,infos)
                 }
             }
+        }
+    }
+}
 
+@Composable
+fun Information(innerPadding:PaddingValues,infos:Infos){
+
+    val ctx = LocalContext.current
+
+    Column(modifier = Modifier.padding(innerPadding).padding(16.dp)) {
+        Text(text = stringResource(R.string.last_name) + " " + (infos.last_name?: ""))
+        Text(text = stringResource(R.string.first_name)+ " " + (infos.first_name?: ""))
+        Text(text = stringResource(R.string.age)+ " " + (infos.age?: ""))
+        Text(text = stringResource(R.string.domain)+ " " + (infos.domain?: ""))
+        Text(text = stringResource(R.string.phone)+ " " + (infos.phone?: ""))
+
+        val intent1 = Intent(ctx,MainActivity::class.java).apply{}
+        val intent2 = Intent(ctx,ThirdActivity::class.java).apply{
+            putExtra("lastname",infos.last_name)
+            putExtra("firstname",infos.first_name)
+            putExtra("age",infos.age)
+            putExtra("domain",infos.domain)
+            putExtra("phone",infos.phone)
+        }
+
+        Button(onClick = {
+            ctx.startActivity(intent2)
+        }) {
+            Text(text = stringResource(R.string.ok_button))
+        }
+        Button(onClick = {
+            ctx.startActivity(intent1)
+        }) {
+            Text(text = stringResource(R.string.return_button))
         }
     }
 }
--- InterfaceSimple2/app/src/main/java/com/example/interfacesimple2/ThirdActivity.kt
+++ InterfaceSimple2/app/src/main/java/com/example/interfacesimple2/ThirdActivity.kt
@@ -8,48 +8,71 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.core.content.ContextCompat
 import com.example.interfacesimple2.ui.theme.InterfaceSimple2Theme
 
 class ThirdActivity : ComponentActivity() {
-
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
 
             val phone = intent.getStringExtra("phone")
 
             InterfaceSimple2Theme {
-                CenteredImage(phone!!)
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text(text = stringResource(R.string.third_title))
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    InterfaceAppel(innerPadding,phone!!)
+                }
             }
         }
     }
 }
 
 @Composable
-fun CenteredImage(p:String) {
+fun InterfaceAppel(innerPadding:PaddingValues,p:String) {
     val ctx = LocalContext.current
     val perm = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission()
@@ -60,7 +83,7 @@ fun CenteredImage(p:String) {
     }
 
     Column (
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.padding(innerPadding).padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
